61502ef33585962009cedad36bd7fa80e540e1e3
André Santos Silva <andrebit5@hotmail.com>
2022-09-15 21:16:39 -0300

    exemplos contatenação

---
+++ exemplo-04.php
@@ -0,0 +1,36 @@
+<?php
+
+
+$nome1 = "André";
+$sobrenome = "Santos Silva";
+$nomeCompleto = $nome1." ".$sobrenome; //cotatenação ) junção de duas variaveis as "" com espaço no meio para dar espaço
+
+
+
+echo $nomeCompleto;
+
+
+exit; //para parar o codigo
+
+echo "<br/>";
+
+//unset($nome1);
+
+if (isset($nome1)) {
+
+}
+
+echo $nome1;
+
+
+
+
+
+
+
+
+
+
+
+
+?>
