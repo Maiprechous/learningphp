d6f5112ccd4cae3a8462f43f4eaf1b200b43a087
André Santos Silva <andrebit5@hotmail.com>
2022-09-15 21:41:33 -0300

    atualizado

---
+++ exemplo-03.php
@@ -0,0 +1,30 @@
+<?php
+
+
+$nome1 = "André";
+
+
+echo $nome1;
+
+echo "<br/>";
+
+//unset($nome1); // excluir a variavel
+
+if (isset($nome1)) { // verifica se a variavel está criada
+
+}
+
+echo $nome1;
+
+
+
+
+
+
+
+
+
+
+
+
+?>
